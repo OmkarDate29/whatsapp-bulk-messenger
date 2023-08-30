cdb6cd7d3ff20450efc2ded81f3686bdef1f8385
Omkar Date <omkardate29@gmail.com>
2023-08-30 15:43:06 +0530

    working

--- automator.py
+++ automator.py
@@ -66,14 +66,14 @@ for idx, number in enumerate(numbers):
 			if not sent:
 				driver.get(url)
 				try:
-					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='compose-btn-send']")))
+					click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button')))
 				except Exception as e:
 					print(style.RED + f"\nFailed to send message to: {number}, retry ({i+1}/3)")
 					print("Make sure your phone and computer is connected to the internet.")
 					print("If there is an alert, please dismiss it." + style.RESET)
 				else:
 					sleep(1)
-					click_btn.click()
+					driver.execute_script("arguments[0].click();", click_btn)
 					sent=True
 					sleep(3)
 					print(style.GREEN + 'Message sent to: ' + number + style.RESET)
